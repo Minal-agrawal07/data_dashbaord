0c8083b9a31f3901b5011718294773e4333a611c
Minal Agrawal <agrawalminal439@gmail.com>
2026-07-17 15:19:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,10 @@ app = FastAPI(title="AI Dashboard Builder", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://data-dashbaord-frontend.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
