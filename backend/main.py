265646cd70edbc4834d2ce0b3d158d9c1580d580
Minal Agrawal <agrawalminal439@gmail.com>
2026-07-22 13:13:34 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -44,13 +44,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://data-dashboard-frontend.onrender.com",
+        "https://data-dashbaord-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # ─── Models ───────────────────────────────────────────────────────────────────
 
